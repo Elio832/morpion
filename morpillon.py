d981e72b3c0937a642e06adc11b9d001f5d8bb6e
elio.peron <elioperon83@gmail.com>
2025-02-04 09:47:13 +0100

    on reviens comme au début enfaite la boucle est bouclé

--- morpillon.py
+++ morpillon.py
@@ -24,7 +24,7 @@ def est_pleine(grille):
 
 def morpion():
     grille = [[" " for _ in range(3)] for _ in range(3)]
-    joueur = "C==>"
+    joueur = "X"
 
     while True:
         afficher_grille(grille)
@@ -57,7 +57,7 @@ def morpion():
             print("Match nul !")
             break
 
-        joueur = "<3" if joueur == "X" else "X"
+        joueur = "0" if joueur == "X" else "X"
 
 if __name__ == "__main__":
     morpion()
